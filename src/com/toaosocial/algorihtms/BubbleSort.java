543bf85f99a5dd32e6ead4d8015476556e8979bd
Zülfikar Karakaya <zulfikarakaya@gmail.com>
2023-07-11 20:21:24 +0200

    Problem : Find the biggest positive integer which not in given array

---
+++ src/com/toaosocial/algorihtms/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.toaosocial.algorihtms;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	static int[] bubbleSort(int[] arr) {
+		int n = arr.length;
+		int temp = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if (arr[j - 1] > arr[j]) {
+					// swap elements
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
+				}
+
+			}
+		}
+		
+		return arr;
+
+	}
+	
+	
+}
--- src/com/toaosocial/problems/FindTheBiggestIntegerNotInGıvenArray.java
+++ src/com/toaosocial/problems/FindTheBiggestIntegerNotInGıvenArray.java
@@ -1,62 +1,46 @@
-package com.toaosocial.algorihtms;
+package com.toaosocial.problems;
 
-public class Solution {
+
+/**
+ * Problem : Find the biggest positive integer which not in given array
+ * 
+ */
+public class FindTheBiggestIntegerNotInGıvenArray {
 
 	public static void main(String[] args) {
+		// test data
 		int[] a = { 1, 3, 6, 4, 1, 2 };
 		int[] b = { 1, 2, 3 };
 		int[] c = { -1, -3 };
 		
-		Solution solution = new Solution();
+		
+		FindTheBiggestIntegerNotInGıvenArray solution = new FindTheBiggestIntegerNotInGıvenArray();
+		
 		System.out.println(solution.solution(a));
 		System.out.println(solution.solution(b));
 		System.out.println(solution.solution(c));
 	}
 
-	public int solution(int[] A) {
+	
+	/**
+	 * 
+	 * @param testDataArray
+	 * @return
+	 */
+	public int solution(int[] testDataArray) {
 
 		int smallest = 1;
 		
-		for (int i = 0; i < A.length; i++) {
-			if (A[i] > 0) { 
-				if ( ((A[i] - smallest) > 1) || ((A[i] - smallest) == 0) )
+		for (int i = 0; i < testDataArray.length; i++) {
+			if (testDataArray[i] > 0) { 
+				if ( ((testDataArray[i] - smallest) > 1) || ((testDataArray[i] - smallest) == 0) )
 					smallest = smallest + 1;
-				else if ((A[i] - smallest) == 1)
-					smallest = A[i] + 1;				
+				else if ((testDataArray[i] - smallest) == 1)
+					smallest = testDataArray[i] + 1;				
 			}
 		}
 
 		return smallest;
 	}
 
-//	public int solution(int[] A) {
-//		
-//		if (A[A.length-1] < 1) return 1;
-//		
-//		for (int i = 1; i < A.length; i++) {
-//			if ((A[i] - A[i-1]) > 1)
-//				return A[i-1] + 1;
-//		}
-//		
-//		return A[A.length-1] + 1;
-//	}
-//	
-	static int[] bubbleSort(int[] arr) {
-		int n = arr.length;
-		int temp = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 1; j < (n - i); j++) {
-				if (arr[j - 1] > arr[j]) {
-					// swap elements
-					temp = arr[j - 1];
-					arr[j - 1] = arr[j];
-					arr[j] = temp;
-				}
-
-			}
-		}
-		
-		return arr;
-
-	}
 }
